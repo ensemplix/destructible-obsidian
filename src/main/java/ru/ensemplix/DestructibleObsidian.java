0cfeb0696e89eb61f4fbb523059bd153cdd10370
Vladislavs Golubs <ensirius@ensemplix.ru>
2015-01-15 02:55:22 +0200

    Доработан плагин

--- src/main/java/ru/ensemplix/DestructibleObsidian.java
+++ src/main/java/ru/ensemplix/DestructibleObsidian.java
@@ -1,7 +1,6 @@
 package ru.ensemplix;
 
-import org.bukkit.Material;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,22 +10,46 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class DestructibleObsidian extends JavaPlugin implements Listener {
 
+    /**
+     * Радиус в которм мы ищем блоки обсидана.
+     */
+    private static final int EXPLOSION_RADIUS = 3;
+
+    /**
+     * Сколько нужно взрывов обсидана что бы он сломался.
+     */
+    private static final int OBSIDIAN_DURABILITY = 10;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+    }
+
     @EventHandler
     public void onExplode(EntityExplodeEvent event) {
-        for(Block block : event.blockList()) {
-            if(block.getType() == Material.OBSIDIAN) {
-                if (block.hasMetadata("hits")) {
-                    block.setMetadata("hits", new FixedMetadataValue(this, 1));
-                }
+        Location location = event.getLocation();
+        World world = location.getWorld();
 
-                int hits = block.getMetadata("hits").get(0).asInt();
+        for (int x = -EXPLOSION_RADIUS; x <= EXPLOSION_RADIUS; x++) {
+            for (int y = -EXPLOSION_RADIUS; y <= EXPLOSION_RADIUS; y++) {
+                for (int z = -EXPLOSION_RADIUS; z <= EXPLOSION_RADIUS; z++) {
+                    Block block = world.getBlockAt(location.clone().add(x, y, z));
 
-                if(hits >= 8) {
-                    block.setType(Material.AIR);
-                    block.removeMetadata("hits", this);
-                    block.getWorld().playSound(block.getLocation(), Sound.EXPLODE, 1, 1);
-                } else {
-                    block.setMetadata("hits", new FixedMetadataValue(this, hits + 1));
+                    if (block.getType() == Material.OBSIDIAN) {
+                        if (!block.hasMetadata("hits")) {
+                            block.setMetadata("hits", new FixedMetadataValue(this, 1));
+                        }
+
+                        int hits = block.getMetadata("hits").get(0).asInt();
+
+                        if (hits >= OBSIDIAN_DURABILITY) {
+                            block.breakNaturally();
+                            block.removeMetadata("hits", this);
+                            block.getWorld().playSound(block.getLocation(), Sound.EXPLODE, 1, 1);
+                        } else {
+                            block.setMetadata("hits", new FixedMetadataValue(this, hits + 1));
+                        }
+                    }
                 }
             }
         }
